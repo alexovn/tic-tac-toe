d9df61d2bdbff288ecf77351a53c4d9943808a59
Nikita Alexov <polarbeararmor@gmail.com>
2024-07-28 20:31:32 +0400

    feat: render dynamic border list

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ function Square ({value, onSquareClick}) {
 }
 
 function Board ({ xIsNext, squares, onPlay }) {
-
   const winner = calculateWinner(squares)
   let status
 
@@ -36,24 +35,31 @@ function Board ({ xIsNext, squares, onPlay }) {
     onPlay(nextSquares)
   }
 
+  function BoardList() {
+    const rows = 3
+    const cols = 3
+
+    return (
+      <div>
+        { Array(rows).fill().map((_, rowIndex) => (
+          <div key={rowIndex} className="board-row">
+            { Array(cols).fill().map((_, colIndex) => {
+              const value = rowIndex * cols + colIndex
+
+              return (
+                <Square key={colIndex} value={squares[value]} onSquareClick={() => handleClick(value)} />
+              )
+            })}
+          </div>
+        ))}
+      </div>
+    )
+  }
+
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      <BoardList />
     </>
   )
 }
